0deefa0a1ed9cb53f89cedfc7637731de64d09ac
LWSE13 <leewheeler888@gmail.com>
2024-05-14 01:38:18 +0100

    updated homepage get request to fetch my last 3 posts in descending order

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,14 +1,37 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   if (req.session.loggedIn) {
-    // If user is logged in, render the dashboard
-    const name = req.session.name || ''; // Get the name from session or set it to an empty string
-    res.render('dashboard', { loggedIn: true, name });
+      const name = req.session.name || ''; 
+      res.render('dashboard', { loggedIn: true, name });
   } else {
-    // If user is not logged in, render the homepage
-    res.render('homepage', { loggedIn: false, name: '' }); // Pass loggedIn as false and an empty name
+      try {
+          const postData = await Post.findAll({
+              order: [['date_created', 'DESC']],
+              limit: 3,
+              include: [
+                  {
+                      model: User,
+                      attributes: ['name']
+                  },
+                  {
+                      model: Comment,
+                      attributes: ['content', 'date_created'],
+                      include: {
+                          model: User,
+                          attributes: ['name']
+                      }
+                  }
+              ]
+          });
+
+          const posts = postData.map(post => post.get({ plain: true }));
+
+          res.render('homepage', { posts });
+      } catch (err) {
+          res.status(500).json({message: 'Failed to get posts', error: err});
+      }
   }
 });
 
